a24b43d385441817aceda4a4ef811189fa7440c3
Robson Narcizo <rob.narcizo.27@gmail.com>
2024-07-11 18:35:53 -0300

    docs: Simulando Falhas no Teste

--- __tests__/testes.test.js
+++ __tests__/testes.test.js
@@ -1,3 +1,7 @@
-test('Teste se 1 + 1 é igual a dois', () => { 
+test('Testa se 1 + 1 é igual a dois', () => { 
     expect(1 + 1).toBe(2);
+});
+
+test('Testa se 2 + 2 é igual a 5', () => { 
+    expect(2 + 2).toBe(5);
 });
