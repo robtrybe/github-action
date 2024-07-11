fc79a07113d2bd7a3341aabf19e25f7398ba3fd7
Robson Narcizo <rob.narcizo.27@gmail.com>
2024-07-11 18:55:41 -0300

    docs: Adicionado Trabalho de Informação

--- __tests__/testes.test.js
+++ __tests__/testes.test.js
@@ -2,6 +2,6 @@ test('Testa se 1 + 1 é igual a dois', () => {
     expect(1 + 1).toBe(2);
 });
 
-test('Testa se 2 + 2 é igual a 5', () => { 
-    expect(2 + 2).toBe(5);
+test('Testa se 2 + 2 é igual a 4', () => { 
+    expect(2 + 2).toBe(4);
 });
